f9876a392194aa7271cd92e92b7fba660d8bd6fd
Raghu K <raghuk.vit@gmail.com>
2026-03-30 08:39:59 +0530

    WMP scripts

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -12,16 +12,43 @@ resource "aws_db_subnet_group" "main" {
   }
 }
 
+resource "aws_security_group" "main" {
+
+  name = "wmp-rds-${var.env}"
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "wmp-rds-${var.env}"
+  }
+
+}
+
 resource "aws_db_instance" "main" {
-  allocated_storage    = var.allocated_storage
-  db_name              = "default_dummy"
-  engine               = "postgres"
-  engine_version       = "16.13"
-  instance_class       = "db.t3.micro"
-  username             = "wmpuser"
-  password             = "WmpUser#1234"
-  parameter_group_name = aws_db_parameter_group.main.name
-  skip_final_snapshot  = true
-  db_subnet_group_name = aws_db_subnet_group.main.name
+  #identifier           = "wmp-${var.env}"
+
+  allocated_storage      = var.allocated_storage
+  db_name                = "default_dummy"
+  engine                 = "postgres"
+  engine_version         = "16.13"
+  instance_class         = "db.t3.micro"
+  username               = "wmpuser"
+  password               = "WmpUser#1234"
+  parameter_group_name   = aws_db_parameter_group.main.name
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.main.id]
 }
 
